866478f5209f3f2ebbc20283b3f927319d1f49a4
tkkc <tkkc123456789@gmail.com>
2018-06-01 02:49:18 +0000

    kadai-microposts

--- resources/views/users/favorite.blade.php
+++ resources/views/users/favorite.blade.php
@@ -1,13 +1,13 @@
+@if (count($microposts) > 0)
 <ul class="media-list">
 @foreach ($microposts as $micropost)
-    <?php $user = $micropost->user; ?>
     <li class="media">
         <div class="media-left">
-            <img class="media-object img-rounded" src="{{ Gravatar::src($user->email, 50) }}" alt="">
+            <img class="media-object img-rounded" src="{{ Gravatar::src($micropost->user->email, 50) }}" alt="">
         </div>
         <div class="media-body">
             <div>
-                {!! link_to_route("users.show", $user->name, ["id" => $user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
+                {!! link_to_route("users.show", $micropost->user->name, ["id" => $micropost->user->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
             </div>
             <div>
                 <p>{!! nl2br(e($micropost->content)) !!}</p>
@@ -27,3 +27,4 @@
 @endforeach
 </ul>
 {!! $microposts->render() !!}
+@endif
